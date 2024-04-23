5e0c13757ef0220e9def17178e72a153fa1656ea
alice-lima-dev <125032253+alice-lima-dev@users.noreply.github.com>
2024-04-22 23:19:40 -0300

    primeira atualização

---
+++ projeto_ecommerce/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Merks</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="box-busca">
+                <div class="search-box">
+                    <form method="post" action="#">
+                        <input type="text" class="search-box-input" name="busca" placeholder="Faça sua Pesquisa">
+                        <button class="search-box-button"><img class="search-box-icone icon icon-search" src="https://img.icons8.com/material-outlined/24/search--v1.png" alt="search--v1" /></button>
+                    </form>
+                </div><!-- Search -->
+            </div><!--Box Busca-->
+            <img id="logo-nav" src="img/logo-merks.png">
+            <div id="cadastro">
+                <img id="img_cadastro" src="https://img.icons8.com/ios/50/add-user-male.png" alt="add-user-male" />
+                <p id="p-cadastro">Cadastre-se</p>
+            </div>
+            <div id="carrinho-id">
+                <img id="icone-carrinho" src="https://img.icons8.com/fluency-systems-regular/48/shopping-cart--v1.png" alt="shopping-cart--v1" />
+            </div>
+        </nav>
+        <hr>
+    </header>
+    <div class="informacoes">
+        <div id="departamentos">
+            <img id="img-departamento" src="img/departamentos.png">
+            <a href="#">
+                <p>Todos os Departamentos</p>
+            </a>
+            <a href="#">
+                <p>Society</p>
+            </a>
+            <a href="#">
+                <p>Campo</p>
+            </a>
+            <a href="#">
+                <p>Social</p>
+            </a>
+        </div>
+    </div>
+    <div class="conteiner">
+        <div class="w3-content w3-section" style="max-width:500px">
+            <img class="mySlides" src="img/chuteiras/chuteria-banner.png" style="width:100%">
+            <img class="mySlides" src="img/chuteiras/chuteira1.png" style="width:100%">
+        </div>
+
+        <div class="informações-contato">
+            <div id="wats">
+                <img id="icone-zap" src="https://img.icons8.com/ios/50/whatsapp--v1.png" alt="whatsapp--v1" />
+                <p id="contato">Entre em contato <br>com nossa empresa</p>
+            </div>
+        </div>
+    </div>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
